c66f7d997cd625808ba88293ceace53e4b18157c
DanielSGH <daniel.st2k@gmail.com>
2022-06-30 11:34:10 +0100

    fixed submoduling, clean up background.js

--- public/background.js
+++ public/background.js
@@ -2,16 +2,11 @@ browser.runtime.onInstalled.addListener(async () => {
   const code_challenge = generateRandom(128);
 
   // get token details from API, set token details in localStorage
-  const redirect = await launchAuth(client_id, code_challenge);
+  const redirect = await launchAuth(code_challenge);
   const authcode = redirect.substring(redirect.indexOf('code=') + 5);
-  const params = {
-    'client_id': client_id,
-    'code': authcode,
-    'code_verifier': code_challenge,
-    'grant_type': 'authorization_code',
-  }
+  console.log(redirect);
 
-  fetchToken(params);
+  fetchToken(authcode, code_challenge);
 })
 
 function generateRandom(length) {
@@ -22,31 +17,16 @@ function generateRandom(length) {
   return txt;
 }
 
-async function launchAuth(client_id, code_challenge) {
+async function launchAuth(code_challenge) {
   return await browser.identity.launchWebAuthFlow({
-    url: `https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=${client_id}&code_challenge=${code_challenge}`,
+    url: auth_handler + `/mal/oauthwebflow?challenge=${code_challenge}`,
     interactive: true
   });
 }
 
-function fetchToken(params) {
-  fetch(proxy + 'https://myanimelist.net/v1/oauth2/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: new URLSearchParams(params)
-  })
-  .then(response => response.json())
-  .then(access => {
-    const authresponse = {
-      access_token: access.access_token,
-      expires_in: access.expires_in * 1000 + Date.now(),
-      refresh_token: access.refresh_token,
-      token_type: access.token_type
-    }
-
-    localStorage.setItem('authresponse', JSON.stringify(authresponse));
-  })
-  .catch(error => console.log(error));
+async function fetchToken(authcode, verifier) {
+  const response = await fetch(auth_handler + `/mal/oauth?authcode=${authcode}&verifier=${verifier}`, { method: 'GET' });
+  const authresponse = await response.json();
+  localStorage.setItem('authresponse', response.ok && authresponse.success ? JSON.stringify(authresponse.data) : false);
+  console.log(authresponse);
 }
